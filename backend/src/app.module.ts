fd41c84617f30e509906622b8f2a5318695d3236
Jeon Gyedong <jeongyedong@gmail.com>
2026-01-07 13:34:49 +0900

    fix(backend): enable conditional SSL for database connection

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -25,6 +25,10 @@ import { LobbyModule } from './lobby/lobby.module';
         database: configService.get<string>('DATABASE_NAME'),
         entities: [User],
         synchronize: true, // Only for development
+        ssl:
+          configService.get<string>('DATABASE_SSL') === 'true'
+            ? { rejectUnauthorized: false }
+            : false,
       }),
     }),
     UsersModule,
@@ -34,4 +38,4 @@ import { LobbyModule } from './lobby/lobby.module';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
